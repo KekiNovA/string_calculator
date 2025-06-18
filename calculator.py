1223459d26cccf98e800937aa80f86c7fc4996eb
KekiNovA <pushpak.pachpande@gmail.com>
2025-06-18 20:25:25 +0530

    feat: support sum for two comma-separated numbers

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    if "," in numbers:
+        parts = numbers.split(",")
+        return sum(int(part) for part in parts)
     return int(numbers)
